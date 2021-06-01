90f31d17a29bded5aa01bc854da61b7b8a9e2a42
Saif-Alaa-ElDin <saifalaa95@gmail.com>
2021-06-01 16:42:14 +0200

    Adding Edit Functionality

--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,9 @@
 import { ListItemText, List, ListItem, Button } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import "./Todo.css";
 import db from "./firbase";
 function Todo(props) {
+  const [open, setOpen] = useState(false);
   return (
     <List className="todo__List">
       <ListItem>
@@ -12,12 +13,45 @@ function Todo(props) {
         ></ListItemText>
       </ListItem>
       <Button
+        variant="contained"
+        color="secondary"
         onClick={(event) => {
           db.collection("todos").doc(props.todo.id).delete();
         }}
       >
         Delete
       </Button>
+      <Button variant="contained" color="danger" onClick={() => setOpen(!open)}>
+        Edit
+      </Button>
+      {open ? (
+        <div>
+          <textarea
+            placeholder={props.todo.text}
+            id={props.todo.id}
+            rows="4"
+            cols="100"
+          ></textarea>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              const value = document.getElementById(props.todo.id).value;
+              db.collection("todos").doc(props.todo.id).set(
+                {
+                  text: value,
+                },
+                { merge: true }
+              );
+              setOpen(false);
+            }}
+          >
+            Change
+          </Button>
+        </div>
+      ) : (
+        <></>
+      )}
     </List>
   );
 }
